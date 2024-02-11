ac46096cea23fdf38d791f864b32e7ac44f94f2b
George Erokhin <Georgii070684@gmail.com>
2024-02-11 17:26:49 +0300

    feat: add create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import fs from 'node:fs/promises';
+import path from 'node:path'
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const FOLDER_PATH = path.join(__dirname, 'files');
+const FILE_PATH = path.join(FOLDER_PATH, 'fresh.txt');
+
 const create = async () => {
-    // Write your code here 
+    try {
+        await fs.writeFile(FILE_PATH, 'I am fresh and young', { flag: 'w' });
+    } catch(e) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await create();
