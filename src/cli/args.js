67d84ffa97cb544cb5bd62097ed98f96f3a76668
George Erokhin <Georgii070684@gmail.com>
2024-02-19 10:02:24 +0300

    feat: add arg.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,15 @@
-const parseArgs = () => {
-    // Write your code here 
-};
+function parseArgs() {
+    let args = process.argv;
+    console.log(process.argv)
+    for (let i = 0; i < args.length; i += 2) {
+        let propName = args[i].replace(/^--/, "");
+        let propValue = args[i + 1];
+        console.log(`${propName} is ${propValue}`);
+    }
+}
 
 parseArgs();
+
+
+
+
