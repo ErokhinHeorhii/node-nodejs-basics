878f8576f0f9f8fe7c17deebc53b2e7708f5b694
George Erokhin <Georgii070684@gmail.com>
2024-02-19 12:02:52 +0300

    feat: add readStreams.js

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,22 @@
+import { createReadStream } from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const file_to_read = path.join(__dirname, 'files', 'fileToRead.txt');
 const read = async () => {
-    // Write your code here 
+        const readStream = createReadStream(file_to_read);
+        readStream.on('data', (chunk) => {
+            process.stdout.write(chunk);
+        });
+        readStream.on('end', () => {
+            process.stdout.write('\n');
+        });
+        readStream.on('error', (err) => {
+            console.error(err);
+        });
 };
 
 await read();
