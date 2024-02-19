80babc2dd6ce42361245f20e1bcf01336cac019b
George Erokhin <Georgii070684@gmail.com>
2024-02-19 13:00:29 +0300

    feat: add writeStreams.js

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,23 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url';
+import fs from 'node:fs';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const FOLDER_PATH = path.join(__dirname, 'files');
+const FILE_PATH = path.join(FOLDER_PATH, 'fileToWrite.txt');
+
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream(FILE_PATH)
+    process.stdin.pipe(writeStream);
+    writeStream.on('finish', () => {
+        console.log('File written OK.');
+    });
+    writeStream.on('error', (err) => {
+        console.error(err.message);
+    });
 };
 
 await write();
