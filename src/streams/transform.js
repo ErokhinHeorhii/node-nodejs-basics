6b0276b9ea3097bf3294e248d67aae8bd6177229
George Erokhin <Georgii070684@gmail.com>
2024-02-19 13:22:52 +0300

    feat: add transformStreams.js

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,24 @@
+import path from 'node:path';
+import {fileURLToPath} from 'node:url';
+import {Transform} from 'stream';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const transform = async () => {
-    // Write your code here 
+    const transformReverce = new Transform(
+        {
+            transform(chunk, encod, fn) {
+                const reverce = chunk.toString().split('').reverse().join('')
+                this.push(reverce)
+                fn()
+                console.log(reverce)
+            }
+        }
+    )
+    process.stdin.pipe(transformReverce).pipe(process.stdout);
+    console.log(process.stdout)
 };
 
 await transform();
