c5bcb85ddb6372040396bc6ccbcebfdd393ddbf7
George Erokhin <Georgii070684@gmail.com>
2024-02-19 10:36:00 +0300

    feat: add esm.js

--- src/modules/esm.mjs
+++ src/modules/esm.mjs
@@ -1,16 +1,20 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import path from 'node:path';
+import {release, version} from 'node:os';
+import {createServer as createServerHttp} from 'node:http';
+import {fileURLToPath} from 'node:url'
+import './files/c.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+    unknownObject = await import('./files/a.json', {with: {type: 'json'}}).then(module => module.default);
 } else {
-    unknownObject = require('./files/b.json');
+    unknownObject = await import ('./files/b.json', {with: {type: 'json'}}).then(module => module.default);
 }
 
 console.log(`Release ${release()}`);
@@ -33,8 +37,5 @@ myServer.listen(PORT, () => {
     console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
+export { unknownObject, myServer}
 
