4824dd67eb40a28d98228380d66bb19fc93f0e0c
George Erokhin <Georgii070684@gmail.com>
2024-02-19 11:35:17 +0300

    feat: add calcHash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,24 @@
+import fs from 'node:fs/promises';
+import { createHash } from 'node:crypto';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const file_path = path.join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+const hashFor = createHash('sha256');
+
+console.log(__filename, __dirname)
 const calculateHash = async () => {
-    // Write your code here 
+    try {
+        const input = await fs.readFile(file_path, 'utf8');
+        const hashFor = createHash('sha256')
+        hashFor.update(input)
+        const digest = hashFor.digest('hex');
+        console.log(digest)
+    } catch (err) {
+        console.error(err);
+    }
 };
 
 await calculateHash();
