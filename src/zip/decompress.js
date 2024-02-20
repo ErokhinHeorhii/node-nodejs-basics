09cff6b68b99597bd39107d7e9469547131585fe
George Erokhin <Georgii070684@gmail.com>
2024-02-20 11:26:15 +0300

    feat: add worker.js

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'node:path';
+import { createGunzip } from 'zlib';
+import { fileURLToPath } from 'node:url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const file_to_comp = path.join(__dirname, 'files', 'fileToCompress.txt');
+const archive_file = path.join(__dirname, 'files', 'archive.gz');
+
+
+
 const decompress = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream(file_to_comp);
+    const readStream = fs.createReadStream(archive_file);
+    const unZip = createGunzip();
+    readStream.pipe(unZip).pipe(writeStream)
 };
 
 await decompress();
